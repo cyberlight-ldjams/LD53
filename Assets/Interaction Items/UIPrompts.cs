ea8f1c1fc985c44e68c5c6d7880f278fcf86dfe5
Mike Knooihuisen <mknooihuisen@gmail.com>
2023-05-01 20:10:02 -0400

    WIP

---
+++ Assets/Interaction Items/UIPrompts.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIPrompts : MonoBehaviour
+{
+    [Header("Interaction Canvas Elements")]
+
+    [SerializeField]
+    private TextMeshProUGUI primaryKey, secondaryKey, primaryPrompt, secondaryPrompt;
+
+    private CanvasGroup primary, secondary, progressGroup;
+
+    [SerializeField]
+    private float fadeSpeed = 0.25f;
+
+    [SerializeField]
+    private Image progressBar;
+
+    private float primaryAlpha = 0f;
+    private float secondaryAlpha = 0f;
+
+    private void Awake()
+    {
+        primary = primaryKey.GetComponentInParent<CanvasGroup>();
+        secondary = secondaryKey.GetComponentInParent<CanvasGroup>();
+        progressGroup = progressBar.GetComponentInParent<CanvasGroup>();
+    }
+
+    public void Update()
+    {
+        if(primary.alpha != primaryAlpha) { 
+            primary.alpha = Mathf.Lerp(primary.alpha, primaryAlpha, fadeSpeed);
+        }
+
+        if (secondary.alpha != secondaryAlpha)
+        {
+            secondary.alpha = Mathf.Lerp(secondary.alpha, primaryAlpha, fadeSpeed);
+        }
+    }
+
+    public void SetPrompt(Prompt p, string key, string text)
+    {
+        if(p == Prompt.PRIMARY)
+        {
+            primaryKey.text = key;
+            primaryPrompt.text = text;
+            primaryAlpha = 1f;
+        } else
+        {
+            secondaryKey.text = key;
+            secondaryPrompt.text = text;
+            secondaryAlpha = 1f;
+        }
+
+    }
+
+    public void ClearPrompt(Prompt p)
+    {
+        if(p == Prompt.PRIMARY)
+        {
+            primaryAlpha = 0f;
+        } else
+        {
+            secondaryAlpha = 0f;
+        }
+    }
+
+    public void SetProgress(float progress) { 
+        progressBar.fillAmount = Mathf.Clamp01(progress);
+        progressGroup.alpha = (progress < 0f && progress < 1f) ? 1f : 0f;
+    }
+
+    public enum Prompt
+    {
+        PRIMARY,
+        SECONDARY
+    }
+}
